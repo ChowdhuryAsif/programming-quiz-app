bbeeea74b975b80c5133d33fa19dbecb5868146d
BS490 <chowdhury.asif.cse@gmail.com>
2021-01-29 18:53:40 +0600

    fixed issues & added listView

--- app/src/main/java/com/example/programming_quiz_app/QuizActivity.java
+++ app/src/main/java/com/example/programming_quiz_app/QuizActivity.java
@@ -3,12 +3,32 @@ package com.example.programming_quiz_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    ListView quizListView;
+    List<String> quizList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        quizListView = findViewById(R.id.quizListView);
+        quizList = Arrays.asList(new DateFormatSymbols().getMonths());
+
+        ArrayAdapter<String> quizAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, quizList);
+        quizListView.setAdapter(quizAdapter);
     }
 }
--- app/src/main/java/com/example/programming_quiz_app/models/Quiz.java
+++ app/src/main/java/com/example/programming_quiz_app/models/Quiz.java
@@ -5,5 +5,33 @@ import java.util.List;
 public class Quiz {
 
     private int id;
+    private String title;
     private List<Question> questionList;
+
+    public Quiz(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Quiz(int id, String title, List<Question> questionList) {
+        this.id = id;
+        this.title = title;
+        this.questionList = questionList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
 }
