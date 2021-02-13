fb8c8e23aa2604850b7ca92a7bcc8108ed7d61e9
unknown <2017100000010@seu.edu.bd>
2021-02-13 22:58:13 +0600

    Wrong dialog functionality implemented

--- app/src/main/java/com/example/programming_quiz_app/WrongDialog.java
+++ app/src/main/java/com/example/programming_quiz_app/WrongDialog.java
@@ -2,13 +2,49 @@ package com.example.programming_quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class WrongDialog extends AppCompatActivity {
+public class WrongDialog {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wrong_dialog);
+    private Context mContext;
+    private Dialog wrongAnswerDialog;
+
+    private QuizActivity mquizActivity;
+
+
+    WrongDialog(Context mContext) {
+        this.mContext = mContext;
     }
+
+    void WrongDialog(String correctAnswer,QuizActivity quizActivity){
+
+        mquizActivity = quizActivity;
+        wrongAnswerDialog = new Dialog(mContext);
+        wrongAnswerDialog.setContentView(R.layout.activity_wrong_dialog);
+        final Button btwrongAnswerDialog = (Button) wrongAnswerDialog.findViewById(R.id.bt_wrongDialog);
+        TextView textView = (TextView) wrongAnswerDialog.findViewById(R.id.textView_Correct_Answer);
+
+        textView.setText("Correct Ans: " + correctAnswer);
+
+        btwrongAnswerDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                wrongAnswerDialog.dismiss();
+                mquizActivity.setQuestionView();
+            }
+        });
+
+        wrongAnswerDialog.show();
+        wrongAnswerDialog.setCancelable(false);
+        wrongAnswerDialog.setCanceledOnTouchOutside(false);
+
+
+    }
+
 }
