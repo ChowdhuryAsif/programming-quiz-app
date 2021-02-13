13d90f49c5e5b2600b050d4fc0fa69de9d013362
unknown <2017100000010@seu.edu.bd>
2021-02-13 23:04:33 +0600

    Timer Dialog functionality implemented

--- app/src/main/java/com/example/programming_quiz_app/TimerDialog.java
+++ app/src/main/java/com/example/programming_quiz_app/TimerDialog.java
@@ -2,13 +2,48 @@ package com.example.programming_quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TimerDialog extends AppCompatActivity {
+public class TimerDialog {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer_dialog);
+    private Context mContext;
+    private Dialog timerDialog;
+
+
+    TimerDialog(Context mContext) {
+        this.mContext = mContext;
     }
+
+    public void timerDialog(){
+
+
+        timerDialog = new Dialog(mContext);
+
+        timerDialog.setContentView(R.layout.activity_timer_dialog);
+        final Button bttimerDialog = (Button) timerDialog.findViewById(R.id.bt_timerDialog);
+
+
+        bttimerDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                timerDialog.dismiss();
+                Intent intent = new Intent(mContext,MainActivity.class);
+                mContext.startActivity(intent);
+
+            }
+        });
+
+        timerDialog.show();
+        timerDialog.setCancelable(false);
+        timerDialog.setCanceledOnTouchOutside(false);
+
+    }
+
+
 }
