8cd96b513de2e6d9f3396a03f744f7534ff3a4cf
AsifChowdhury-bs23 <chowdhury.asif.cse@gmail.com>
2021-02-13 23:14:55 +0600

    correctDialog functionality done

--- app/src/main/java/com/example/programming_quiz_app/CorrectDialog.java
+++ app/src/main/java/com/example/programming_quiz_app/CorrectDialog.java
@@ -1,14 +1,56 @@
 package com.example.programming_quiz_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CorrectDialog extends AppCompatActivity {
+public class CorrectDialog {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_correct_dialog);
+    private Context mContext;
+    private Dialog correctDialog;
+
+    private QuizActivity mquizActivity;
+
+    public CorrectDialog(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void correctDialog(int score,QuizActivity quizActivity){
+
+        mquizActivity = quizActivity;
+
+
+        correctDialog = new Dialog(mContext);
+
+        correctDialog.setContentView(R.layout.activity_correct_dialog);
+        final Button btcorrectDialog = (Button) correctDialog.findViewById(R.id.bt_Score_Dialog);
+
+        Score(score);  //  calling method
+
+        btcorrectDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                correctDialog.dismiss();
+                mquizActivity.setQuestionView();
+
+            }
+        });
+
+        correctDialog.show();
+        correctDialog.setCancelable(false);
+        correctDialog.setCanceledOnTouchOutside(false);
+        correctDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    private void Score(int score) {
+
+        TextView textScore = (TextView) correctDialog.findViewById(R.id.textView_Score);
+        textScore.setText("Score: " + String.valueOf(score));
     }
 }
