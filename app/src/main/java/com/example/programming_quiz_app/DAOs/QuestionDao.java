dd71b08a61281e2e5b8cd75a0c97c5041b6d909d
AsifChowdhury-bs23 <chowdhury.asif.cse@gmail.com>
2021-02-13 13:22:49 +0600

    LiveData issue fixed

--- app/src/main/java/com/example/programming_quiz_app/DAOs/QuestionDao.java
+++ app/src/main/java/com/example/programming_quiz_app/DAOs/QuestionDao.java
@@ -1,10 +1,10 @@
 package com.example.programming_quiz_app.DAOs;
 
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 
 import com.example.programming_quiz_app.models.Questions;
 
