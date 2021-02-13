ece1c9c78b52fa2f2c5405ea0561ee905026c380
AsifChowdhury-bs23 <chowdhury.asif.cse@gmail.com>
2021-02-13 13:36:29 +0600

    sqliteRoomDB added

--- app/src/main/java/com/example/programming_quiz_app/SQLiteRoomDB.java
+++ app/src/main/java/com/example/programming_quiz_app/SQLiteRoomDB.java
@@ -1,10 +1,13 @@
 package com.example.programming_quiz_app;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.programming_quiz_app.DAOs.QuestionDao;
 import com.example.programming_quiz_app.models.Questions;
@@ -12,7 +15,7 @@ import com.example.programming_quiz_app.models.Questions;
 @Database(entities = {Questions.class}, version = 1)
 public abstract class SQLiteRoomDB extends RoomDatabase {
 
-    public abstract QuestionDao quizDAO();
+    public abstract QuestionDao questionDao();
 
     private static volatile SQLiteRoomDB INSTANCE;
 
@@ -27,5 +30,43 @@ public abstract class SQLiteRoomDB extends RoomDatabase {
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback RoomDBCallback = new RoomDatabase.Callback(){
+
+
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+
+            new PopulateDbAsyncTask(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void> {
+
+        private QuestionDao questionDao;
+
+
+        private PopulateDbAsyncTask(SQLiteRoomDB db){
+
+            questionDao = db.questionDao();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids){
+
+
+            questionDao.insert(new Questions("What is Android?","OS","Browser","Software","Hard Drive",1));
+            questionDao.insert(new Questions("RAM Stands for what ?","Operating System","Browser","Random Access Memory","CD Project",3));
+            questionDao.insert(new Questions("Chrome is what ?","System Software","Browser","Middle Ware","Windows",2));
+            questionDao.insert(new Questions("HTML is what ?","Scipting Language","Programming Language","Software","Hyper Text Markup Language",4));
+            questionDao.insert(new Questions("Unity is used for ?","Game Developement","Web Development","Graphics Design","3-D Modling",2));
+            questionDao.insert(new Questions("What is OS","Hardware","System Software","PC Software","Hard Drive",2));
+            questionDao.insert(new Questions("IP stand for what? ","Language","Intenet Protocol","Graphics","Random",2));
+            return null;
+        }
     }
 }
