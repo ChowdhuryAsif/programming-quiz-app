5da6abb821c693d62c4a6c218797792d4fcb8102
AsifChowdhury-bs23 <chowdhury.asif.cse@gmail.com>
2021-02-13 16:54:34 +0600

    designed correct dialog

---
+++ app/src/main/java/com/example/programming_quiz_app/CorrectDialog.java
@@ -0,0 +1,14 @@
+package com.example.programming_quiz_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CorrectDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correct_dialog);
+    }
+}
